9296b8879314f18dcf7bd39fcf322d9f760e6ca3
dominicvogel <davogel2001@gmail.com>
2022-10-31 16:18:13 -0400

    Updated With dotenv

--- app/unemployment.py
+++ app/unemployment.py
@@ -6,6 +6,10 @@ from pprint import pprint
 
 import requests
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
 request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
